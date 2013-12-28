b3bb07b8f553b4ac2f2f046bb2ddf192fd225c3d
Sashko Stubailo <sashko@mit.edu>
2013-12-27 23:50:51 -0500

    Rails 4 initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Napdea::Application.config.secret_key_base = 'de23b3f386b650981010f3536f92840a55f7dc949b4cb2f7a32d1f411125c33ae23ff2cf93fe3c7bab3d0d82ab01cd053b5e280fa0f339407b8dba97d2ffd799'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Napdea::Application.config.session_store :cookie_store, key: '_napdea_session'
