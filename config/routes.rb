f79705cff8ac516d023ac374131eecd993ba2a39
Sashko Stubailo <sashko@mit.edu>
2013-12-28 00:04:25 -0500

    Changed database

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Napdea::Application.routes.draw do
+  resources :signups
+
+  root "signups#new"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
